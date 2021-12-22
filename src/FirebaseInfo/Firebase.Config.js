c6ae1f2ded32221f30d41b9b735e71d77f4d5e43
MHRafix <fmhrafiz27@gmail.com>
2021-12-22 10:59:18 +0600

    code push

--- src/FirebaseInfo/Firebase.Config.js
+++ src/FirebaseInfo/Firebase.Config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAG35QkKCfedtKiMwy85Pah7oLS60HxH3Q",
-    authDomain: "skillup-8f97d.firebaseapp.com",
-    projectId: "skillup-8f97d",
-    storageBucket: "skillup-8f97d.appspot.com",
-    messagingSenderId: "755048749976",
-    appId: "1:755048749976:web:8541963cf1c1fb39e6e9ed"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
   export default firebaseConfig;
